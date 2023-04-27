cbcc92965deb04a765c916f53dbe4346944cc7a1
jan dettler <jan.dettler@hs-osnabrueck.de>
2023-04-27 13:09:26 +0200

    Initial Commit

---
+++ prototypSEP/prototypSEPApp.swift
@@ -0,0 +1,17 @@
+//
+//  prototypSEPApp.swift
+//  prototypSEP
+//
+//  Created by Jan Dettler on 27.04.23.
+//
+
+import SwiftUI
+
+@main
+struct prototypSEPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
